02363f9ff1b67aa7be996e0e153be92af0ceabfa
heronb.dev <heronb.dev@gmail.com>
2023-05-18 09:31:29 -0300

    atualizacao contador par impar

--- lacos05/src/pacote/ExercicioImputParImpar.java
+++ lacos05/src/pacote/ExercicioImputParImpar.java
@@ -1,5 +1,7 @@
 package pacote;
 
+import java.util.Scanner;
+
 public class ExercicioImputParImpar {
 
 	public static void main(String[] args) {
@@ -7,9 +9,20 @@ public class ExercicioImputParImpar {
 		 * contá-los até que seja digitado um numero impar.
 		 * Numeros: 4 8 6 2 2 5
 		 * >> Voce digitou 5 numeros pares*/
+		int cont=0, i=0;
+		boolean par = true;
+		Scanner leitor = new Scanner(System.in);
 		
+		while (par) 
+		{
+			System.out.println("Contador digitacao pares: "+cont);
+			System.out.println("Digite numeros pares pra contar e impar pra sair:");
+			i = leitor.nextInt();
+			par = ((i%2)==0);
+			
 		
-
+		}
+		System.out.println("Total pares: "+cont);
 	}
 
 }
