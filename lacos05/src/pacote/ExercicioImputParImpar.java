74d1f23ba5f4f7729a1cdf7130a3c819dacc220c
heronb.dev <heronb.dev@gmail.com>
2023-05-17 22:11:16 -0300

    inclusao exercicio impar e par

---
+++ lacos05/src/pacote/ExercicioImputParImpar.java
@@ -0,0 +1,15 @@
+package pacote;
+
+public class ExercicioImputParImpar {
+
+	public static void main(String[] args) {
+		/*Dados diversos números pelo usuário, 
+		 * contá-los até que seja digitado um numero impar.
+		 * Numeros: 4 8 6 2 2 5
+		 * >> Voce digitou 5 numeros pares*/
+		
+		
+
+	}
+
+}
