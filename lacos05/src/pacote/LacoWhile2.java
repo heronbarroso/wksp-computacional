df55dc601b1cb2324cf9704e956dbbebe513b8b9
heronb.dev <heronb.dev@gmail.com>
2023-05-17 21:31:19 -0300

    Atualização lacowhile2

---
+++ lacos05/src/pacote/LacoWhile2.java
@@ -0,0 +1,17 @@
+package pacote;
+
+public class LacoWhile2 {
+
+	public static void main(String[] args) {
+		int i=1, multiplo=5, contador=0;
+		
+		while (contador <10) {
+			
+			if (i % multiplo == 0) {
+				System.out.print(i+" ");
+				contador ++;
+			}
+			i++;
+		}
+	}
+}
